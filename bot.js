28b7f740382fe3744b5cc7a130b07c262a958d8d
Hyunakyu <32633228+Hyunakyu@users.noreply.github.com>
2018-09-15 21:52:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,52 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true}); // botnya tidak akan bisa mention @everyone
+const config = require("./config.json"); // tempat prefix dan token disini
+const fs = require("fs");
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const client = new Discord.Client({disableEveryone: true});
+const youtube = new YouTube("AIzaSyBonrJPtHPxsiO2IpX2f9LwiNp2oigc8eQ");
+const queue = new Map();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on("ready", async () => {
+	console.log(`${bot.user.username} Sudah online!`);
+	bot.user.setActivity("-help | -play", {type: "PLAYING"});	
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+bot.on("message", async message => {
+	if (message.author.bot) return; // bot  tidak akan menjawab jika command dikirim oleh bot lain
+	if (message.channel.type === 'dm') return; // bot tidak akan menjawab jika kita menggunakan command di DM atau PM
+
+	let prefix = config.prefix;
+	let msg = message.content.toLowerCase(); //all sensitive:V 
+	let args = message.content.slice(prefix.length).trim().split(" ");
+	let cmd = args.shift().toLowerCase();
+
+	if (!msg.startsWith(prefix)) return;
+
+	try {
+		let commandFile = require(`./command/${cmd}.js`);
+		commandFile.run(bot, message, args);
+	}catch (e) {
+		console.log(e.message)
+	}finally {
+		console.log(`${message.author.username} menggunakan perintah ${cmd}`);
+	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDg3Nzc2MzU5OTY0NDc1NDI4.Dnk47g.P8ZXjvKwh9rSHz9mmCt1xHUXcSY);
+bot.on("guildMemberAdd", member => {
+	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+	if (!autorole[member.guild.id]) { // jika tidak ada autorole yang di set, agar tidak error saat ada yang join
+		autorole[member.guild.id] = {
+			autorole: config.autorole
+		};
+	}
+	var role = autorole[member.guild.id].role;
+	if (role === '0') return; // jika autorole 0 maka akan dihentikan dan tidak menyebabkan error
+	member.addRole(role);
+
+	
+
+});
+
+bot.login(config.token);
